f48ef11cb9ba54ec3550afbf96f2b0d89041b452
JonDev2023 <roupasee@gmail.com>
2023-02-09 17:23:29 -0300

    Window Maked

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ Programmed by JonDev2023
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen, ScreenManager
 
+class ADM(ScreenManager):
+    pass
+
+class Modeling(Screen):
+    pass
+
+class Runner(App):
+    def build(self):
+        self.title = '3DPNG'
+        return Builder.load_file('index.kv')
+
+if __name__ == '__main__':
+    Runner().run()
